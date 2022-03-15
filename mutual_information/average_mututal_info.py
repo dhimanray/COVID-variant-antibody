98e504f9c9f62d7404560a5ea579f94954e9b92c
dhimanray <>
2022-03-15 01:40:26 -0700

    Error analysis update

--- mutual_information/average_mututal_info.py
+++ mutual_information/average_mututal_info.py
@@ -7,4 +7,7 @@ l4 = np.loadtxt('mutual_info_400-500ns.txt')
 
 lavg = 0.25*(l1+l2+l3+l4)
 
+lmse = np.sqrt(0.33*((l1-lavg)**2 + (l2-lavg)**2 + (l3-lavg)**2 + (l4-lavg)**2))
+
 np.savetxt('mutual_info_avg.txt',lavg)
+np.savetxt('mutual_info_err.txt',lmse)
